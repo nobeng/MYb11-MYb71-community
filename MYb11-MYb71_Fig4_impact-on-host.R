ad4f13ebd51ccaec4125d325491aca74b9c5a491
nobeng <nancy.obeng@hotmail.de>
2022-11-03 13:49:33 +0100

    Renamed after shifting data to new figure of the manuscript

--- MYb11-MYb71_Fig4_impact-on-host.R
+++ MYb11-MYb71_Fig4_impact-on-host.R
@@ -5,7 +5,7 @@
 ########################################
 
 #### = 1. Dependencies, directories and variables = ####
-library(plyr); library(ggplot2); library(gridExtra); library(multcomp)
+library(plyr); library(ggplot2); library(gridExtra); library(multcomp); library(ggh4x)
 
 # Set working directory to source location
 currentDirectory <- dirname(rstudioapi::getActiveDocumentContext()$path)
@@ -18,121 +18,126 @@ source("my_theme.R")
 colsTreat <- c("black","#8DB500", "#FFB733")
 #colsMYb <- c("#FC2F62", "grey27", "#189E00", "blue")
 colsMYb <- c("#B9006A", "grey27", "#139150")
+colsTreat <- c("grey", "white", "white")
 assay <- assayName <- "wormSize"
 cycs <- c(4, 10)
 
 #### = 2. Function definitions = ####
 
 # Function to plot worm dimensions as a function of treatE
-plot_wormDimension.treatE <- function(dataset, cycle, cols, strains, dims, labelX, plotH, plotW){
-  # 1. Set cycle to exclude from data set
-  ifelse(cycle == 4, cyc <- 10, ifelse(cycle == 10, cyc <- 4, 0))
-  dataset <- dataset[dataset$cycle != cyc,]
-  
-  # Leave 20% space on top of max. data point for significance stars later
-  top <- max(dataset[,dims])*1.05
-  
-  # 2. Print plot
-  p <- ggplot(dataset, aes(x = toupper(treatE), y = dataset[, dims]))+
-    geom_boxplot(aes(fill = strain), size = 0.7) +
-    geom_jitter(size = 2, alpha = 0.5, width = 0.1)+
-    geom_point(aes(x=1, y=top), alpha=0)+
-    xlab("Life Cycle") + ylab(labelX)+
-    scale_fill_manual(values = cols) +
-    scale_color_manual(values = cols) +
-    facet_grid(~strain)+
-    my_theme(dataset)
-  
-  # 3. Save plot to file
-  # Check presence of directory
-  absPlots <- paste(currentDirectory, "/plots", sep="")
-  ifelse(dir.exists(absPlots) == F, 
-         dir.create(absPlots), "Directory for absolute data already exists.")
-  
-  # Define file name
-  fileExtensions <- c(".svg", ".png")
-  for(i in 1:length(fileExtensions)){
-    fileName <- paste(absPlots, "/", assayName, "_", dims,"~treatE_C", cycle, "_", strains,
-                      fileExtensions[i], sep = "")
-    # Save plot
-    ggsave(fileName, p, height=plotH, width=plotW,dpi=300) }
-  
-  # Check for differences between treatE
-  # Prepare output file name
-  fileName <- paste("stats/monoVSco_",dims, "_compTREATE_ANOVA+Dunnet",".txt", sep="")
-  
-  # Adjust factor levels of strains to test mono against co-culture abundances
-  dataset$strain <- factor(dataset$strain, levels = c("co-culture", "MYb11", "MYb71"))
-  dataset$treatE <- factor(dataset$treatE, levels = c("anc", "mono", "bi"))
-  
-  # Run linear regression (LM with ANOVA)
-  m <- lm(dataset[, dims] ~ treatE*repO, data=dataset)
-  
-  sink(fileName)
-  print(as.data.frame(anova(m))) # ANOVA output
-  print("")
-  print(summary(glht(m, mcp(treatE="Tukey")))) # Post-hoc output: Tukey
-  print("")
-  print(summary(glht(m, mcp(treatE="Dunnett")))) # Post-doc output: Dunnett
-  sink()
-  
-  return(p)  }
+{
+  # plot_wormDimension.treatE <- function(dataset, cycle, cols, strains, dims, labelX, plotH, plotW){
+  # # 1. Set cycle to exclude from data set
+  # ifelse(cycle == 4, cyc <- 10, ifelse(cycle == 10, cyc <- 4, 0))
+  # dataset <- dataset[dataset$cycle != cyc,]
+  # 
+  # # Leave 20% space on top of max. data point for significance stars later
+  # top <- max(dataset[,dims])*1.05
+  # 
+  # # 2. Print plot
+  # p <- ggplot(dataset, aes(x = toupper(treatE), y = dataset[, dims]))+
+  #   geom_boxplot(aes(fill = treatE), size = 0.7) +
+  #   geom_jitter(size = 2, alpha = 0.5, width = 0.1)+
+  #   geom_point(aes(x=1, y=top), alpha=0)+
+  #   xlab("Evol. treatment") + ylab(labelX)+
+  #   scale_fill_manual(values = cols) +
+  #   scale_color_manual(values = cols) +
+  #   #facet_grid(~strain)+
+  #   my_theme(dataset)
+  # 
+  # # 3. Save plot to file
+  # # Check presence of directory
+  # absPlots <- paste(currentDirectory, "/plots", sep="")
+  # ifelse(dir.exists(absPlots) == F, 
+  #        dir.create(absPlots), "Directory for absolute data already exists.")
+  # 
+  # # Define file name
+  # fileExtensions <- c(".svg", ".png")
+  # for(i in 1:length(fileExtensions)){
+  #   fileName <- paste(absPlots, "/", assayName, "_", dims,"~treatE_C", cycle, "_", strains,
+  #                     fileExtensions[i], sep = "")
+  #   # Save plot
+  #   ggsave(fileName, p, height=plotH, width=plotW,dpi=300) }
+  # 
+  # # Check for differences between treatE
+  # # Prepare output file name
+  # fileName <- paste("stats/monoVSco_",dims, "~treatE_ANOVA+Tukey_and_Dunnett",".txt", sep="")
+  # 
+  # # Adjust factor levels of strains to test mono against co-culture abundances
+  # dataset$strain <- factor(dataset$strain, levels = c("co-culture", "MYb11", "MYb71"))
+  # dataset$treatE <- factor(dataset$treatE, levels = c("anc", "mono", "bi"))
+  # 
+  # # Run linear regression (LM with ANOVA)
+  # m <- lm(dataset[, dims] ~ treatE+repO, data=dataset)
+  # 
+  # sink(fileName)
+  # print(anova(m)) # ANOVA output
+  # print("")
+  # print(summary(glht(m, mcp(treatE="Tukey")))) # Post-hoc output: Tukey
+  # print("")
+  # print(summary(glht(m, mcp(treatE="Dunnett")))) # Post-doc output: Dunnett
+  # sink()
+  # 
+  # return(p)  }
+}
 
 # Function to plot worm dimensions of ancestors
-plot_wormDimension.strainANC <- function(dataset, cycle, cols, strains, dims, labelX, plotH, plotW){
-  # 1. Set cycle to exclude from data set
-  ifelse(cycle == 4, cyc <- 10, ifelse(cycle == 10, cyc <- 4, 0))
-  dataset <- dataset[dataset$cycle != cyc,]
-  
-  # Leave 20% space on top of max. data point for significance stars later
-  top <- max(dataset[,dims])*1.05
-  
-  dataset$strain <- factor(dataset$strain, levels = c("MYb11", "MYb71", "co-culture"))
-  
-  # 2. Print plot
-  p <- ggplot(dataset[dataset$treatE == "anc", ], aes(x = strain, y = dataset[, dims]))+
-    geom_boxplot(aes(fill = strain), size = 0.7) +
-    geom_jitter(size = 2.5, alpha = 0.5, width = 0.1)+
-    geom_point(aes(x=1, y=top), alpha=0)+
-    xlab("Bacteria") + ylab(labelX)+
-    scale_fill_manual(values = cols[c(1, 3, 2)]) +
-    scale_color_manual(values = cols[c(1, 3, 2)]) +
-    #facet_grid(~strain)+
-    my_theme(dataset)+
-    theme(axis.text.x = element_text(hjust = 1, angle = 45))
-  
-  # 3. Save plot to file
-  # Check presence of directory
-  absPlots <- paste(currentDirectory, "/plots", sep="")
-  ifelse(dir.exists(absPlots) == F, 
-         dir.create(absPlots), "Directory for absolute data already exists.")
-  
-  # Define file name
-  fileExtensions <- c(".svg", ".png")
-  for(i in 1:length(fileExtensions)){
-    fileName <- paste(absPlots, "/", assayName, "_", dims,"~strain_ANCESTORS_", strains, fileExtensions[i], sep = "")
-    # Save plot
-    ggsave(fileName, p, height=plotH, width=plotW,dpi=300) }
-  
-  # Check for differences between either monoculture and the co-culture within each treatE
-  # Prepare output file name
-  fileName <- paste("stats/monoVSco_", dims, "_compANC_ANOVA+Dunnet",".txt", sep="")
-  
-  # Adjust factor levels of strains to test mono against co-culture abundances
-  dataset$strain <- factor(dataset$strain, levels = c("co-culture", "MYb11", "MYb71"))
-  
-  # Run linear regression (LM with ANOVA)
-  m <- lm(dataset[, dims] ~ strain*repO, data=dataset)
-  
-  sink(fileName)
-  print(as.data.frame(anova(m))) # ANOVA output
-  print("")
-  print(summary(glht(m, mcp(strain="Tukey")))) # Post-hoc output: Tukey
-  print("")
-  print(summary(glht(m, mcp(strain="Dunnett")))) # Post-doc output: Dunnett
-  sink()
-  
-  return(p)  }
+{
+  # plot_wormDimension.strainANC <- function(dataset, cycle, cols, strains, dims, labelX, plotH, plotW){
+  # # 1. Set cycle to exclude from data set
+  # ifelse(cycle == 4, cyc <- 10, ifelse(cycle == 10, cyc <- 4, 0))
+  # dataset <- dataset[dataset$cycle != cyc,]
+  # 
+  # # Leave 20% space on top of max. data point for significance stars later
+  # top <- max(dataset[,dims])*1.05
+  # 
+  # dataset$strain <- factor(dataset$strain, levels = c("MYb11", "MYb71", "co-culture"))
+  # 
+  # # 2. Print plot
+  # p <- ggplot(dataset[dataset$treatE == "anc", ], aes(x = strain, y = dataset[, dims]))+
+  #   geom_boxplot(aes(fill = strain), size = 0.7) +
+  #   geom_jitter(size = 2.5, alpha = 0.5, width = 0.1)+
+  #   geom_point(aes(x=1, y=top), alpha=0)+
+  #   xlab("Bacteria") + ylab(labelX)+
+  #   scale_fill_manual(values = cols[c(1, 3, 2)]) +
+  #   scale_color_manual(values = cols[c(1, 3, 2)]) +
+  #   #facet_grid(~strain)+
+  #   my_theme(dataset)+
+  #   theme(axis.text.x = element_text(hjust = 1, angle = 45))
+  # 
+  # # 3. Save plot to file
+  # # Check presence of directory
+  # absPlots <- paste(currentDirectory, "/plots", sep="")
+  # ifelse(dir.exists(absPlots) == F, 
+  #        dir.create(absPlots), "Directory for absolute data already exists.")
+  # 
+  # # Define file name
+  # fileExtensions <- c(".svg", ".png")
+  # for(i in 1:length(fileExtensions)){
+  #   fileName <- paste(absPlots, "/", assayName, "_", dims,"~strain_ANCESTORS_", strains, fileExtensions[i], sep = "")
+  #   # Save plot
+  #   ggsave(fileName, p, height=plotH, width=plotW,dpi=300) }
+  # 
+  # # Check for differences between either monoculture and the co-culture within each treatE
+  # # Prepare output file name
+  # fileName <- paste("stats/monoVSco_", dims, "_compANC_ANOVA+Dunnet",".txt", sep="")
+  # 
+  # # Adjust factor levels of strains to test mono against co-culture abundances
+  # dataset$strain <- factor(dataset$strain, levels = c("co-culture", "MYb11", "MYb71"))
+  # 
+  # # Run linear regression (LM with ANOVA)
+  # m <- lm(dataset[, dims] ~ strain+repO, data=dataset)
+  # 
+  # sink(fileName)
+  # print(anova(m)) # ANOVA output
+  # print("")
+  # print(summary(glht(m, mcp(strain="Tukey")))) # Post-hoc output: Tukey
+  # print("")
+  # print(summary(glht(m, mcp(strain="Dunnett")))) # Post-doc output: Dunnett
+  # sink()
+  # 
+  # return(p)  }
+}
 
 # Function to plot # washed worms detected as a function of treatE
 plot_nWorms.treatE <- function(dataset, cycle, cols, strains,plotH, plotW){
@@ -146,13 +151,14 @@ plot_nWorms.treatE <- function(dataset, cycle, cols, strains,plotH, plotW){
   # 2. Print plot
   p <- ggplot(dataset, aes(x = toupper(treatE), y = nWorms))+
     geom_boxplot(aes(fill = strain), size = 0.7) +
-    geom_jitter(size = 2, alpha = 0.5, width = 0.1)+
+    geom_jitter(size = 2.5, alpha = 0.5, width = 0.1)+
     geom_point(aes(x=1, y=top), alpha=0)+
-    xlab("Life Cycle") + ylab("# worms/150?l")+
+    xlab("Evol. treatment") + ylab("Population size (# worms)")+
     scale_fill_manual(values = cols) +
     scale_color_manual(values = cols) +
-    facet_grid(~strain)+
-    my_theme(dataset)
+    my_theme(dataset)+
+    force_panelsizes(rows = unit(2.15, "in"),
+                     cols = unit(1.3, "in"))
   
   # 3. Save plot to file
   # Check presence of directory
@@ -170,16 +176,16 @@ plot_nWorms.treatE <- function(dataset, cycle, cols, strains,plotH, plotW){
   
   # Check for differences between treatE
   # Prepare output file name
-  fileName <- paste("stats/monoVSco_nWorms_compTREATE_ANOVA+Dunnet",".txt", sep="")
+  fileName <- paste("stats/monoVSco_nWorms_compTREATE_ANOVA_and_Dunnett",".txt", sep="")
   
   # Adjust factor levels of strains to test mono against co-culture abundances
   dataset$treatE <- factor(dataset$treatE, levels = c("anc", "mono", "bi"))
   
   # Run linear regression (LM with ANOVA)
-  m <- lm(nWorms ~ treatE*repO, data=dataset)
+  m <- lm(nWorms ~ treatE+repO, data=dataset)
   
   sink(fileName)
-  print(as.data.frame(anova(m))) # ANOVA output
+  print(anova(m)) # ANOVA output
   print("")
   print(summary(glht(m, mcp(treatE="Tukey")))) # Post-hoc output: Tukey
   print("")
@@ -203,13 +209,15 @@ plot_nWorms.strainANC <- function(dataset, cycle, cols, strains,plotH, plotW){
   # 2. Print plot
   p <- ggplot(dataset, aes(x = strain, y = nWorms))+
     geom_boxplot(aes(fill = strain), size = 0.7) +
-    geom_jitter(size = 2, alpha = 0.5, width = 0.1)+
+    geom_jitter(size = 2.5, alpha = 0.5, width = 0.1)+
     geom_point(aes(x=1, y=top), alpha=0)+
-    xlab("Bacteria") + ylab("# worms/150?l")+
+    xlab("Bacteria") + ylab("Population size (# worms)")+
     scale_fill_manual(values = cols[c(1,3,2)]) +
     scale_color_manual(values = cols[c(1,3,2)]) +
     my_theme(dataset)+
-    theme(axis.text.x = element_text(hjust = 1, angle = 45))
+    theme(axis.text.x = element_text(hjust = 1, angle = 45))+
+    force_panelsizes(rows = unit(2.15, "in"),
+                     cols = unit(1.3, "in"))
   
   # 3. Save plot to file
   # Check presence of directory
@@ -227,16 +235,16 @@ plot_nWorms.strainANC <- function(dataset, cycle, cols, strains,plotH, plotW){
   
   # Check for differences between either monoculture and the co-culture within each treatE
   # Prepare output file name
-  fileName <- paste("stats/monoVSco_nWorms_compANC_ANOVA+Dunnet",".txt", sep="")
+  fileName <- paste("stats/monoVSco_nWorms_compANC_ANOVA_and_Dunnett",".txt", sep="")
   
   # Adjust factor levels of strains to test mono against co-culture abundances
   dataset$strain <- factor(dataset$strain, levels = c("co-culture", "MYb11", "MYb71"))
   
   # Run linear regression (LM with ANOVA)
-  m <- lm(nWorms ~ strain*repO, data=dataset)
+  m <- lm(nWorms ~ strain+repO, data=dataset)
   
   sink(fileName)
-  print(as.data.frame(anova(m))) # ANOVA output
+  print(anova(m)) # ANOVA output
   print("")
   print(summary(glht(m, mcp(strain="Tukey")))) # Post-hoc output: Tukey
   print("")
@@ -337,17 +345,18 @@ labelsX <- c(expression("Area (mean mm"^"2"*")"),
 c <- 2 # focus on evolved cycle 10 material and ancestor
   for (d in 1:length(wormDimensions)){ # worm dimensions measured
     # main figure
-    plot_wormDimension.treatE(dataMeans[dataMeans$strain == "co-culture",], cycs[c], colsMYb, "co-culture", wormDimensions[d],labelsX[d], 3.2, 2.5)
-    plot_nWorms.treatE(dataMeans[dataMeans$strain == "co-culture",], cycs[c], colsMYb, "co-culture", 3.2, 2.5)
+  # plot_wormDimension.treatE(dataMeans[dataMeans$strain == "co-culture",], cycs[c], colsTreat, "co-culture", wormDimensions[d],labelsX[d], 3.2, 2.5)
+    plot_nWorms.treatE(dataMeans[dataMeans$strain == "co-culture",], cycs[c], colsTreat, "co-culture", 3.2, 2.5)
     
-    plot_wormDimension.strainANC(dataMeans[dataMeans$treatE == "anc",],
-                              cycs[c], colsMYb, "ANC_comparisons", wormDimensions[d],labelsX[d], 3.2, 2.5)
+   # plot_wormDimension.strainANC(dataMeans[dataMeans$treatE == "anc",],
+                              #cycs[c], colsMYb, "_ANC_comparisons", wormDimensions[d],labelsX[d], 3.2, 2.5)
     plot_nWorms.strainANC(dataMeans[dataMeans$treatE == "anc",],
-                       cycs[c], colsMYb, "ANC_comparisons", 3.2, 2.5)
+                       cycs[c], colsMYb, "_ANC_comparisons", 3.2, 2.5)
     
     # supplement
-    plot_wormDimension.treatE(dataMeans,cycs[c], colsMYb, "MYb", wormDimensions[d],labelsX[d], 3, 7)
-    plot_nWorms.treatE(dataMeans, cycs[c], colsMYb, "MYb", 3, 7) }#}
+    #plot_wormDimension.treatE(dataMeans,cycs[c], colsMYb, "MYb", wormDimensions[d],labelsX[d], 3, 7)
+    #plot_nWorms.treatE(dataMeans, cycs[c], colsMYb, "MYb", 3, 7) }
+    }
 
 
 #### = Clean up = ####
